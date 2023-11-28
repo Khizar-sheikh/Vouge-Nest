74f547afd3faee1d48965c4371e167cd22d62c3c
Khizar Sheikh <khizarsheikh.012@gmail.com>
2023-11-28 22:02:36 +0500

    Delete Route :  Order

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import Mens from "./assets/Components/Pages/mens/Mens";
 import Womens from "./assets/Components/Pages/Womens/Womens";
 import Velocity from "./assets/Components/Pages/Velocity/Velocity";
 import Cart from "./assets/Components/Pages/cart/cart";
-import Order from "./assets/Components/Pages/order/Order";
 import MyAccount from "./assets/Components/Pages/Account/MyAccount";
 import Contact from "./assets/Components/Pages/contact/contact";
 import Job from "./assets/Components/Pages/Jobs/job";
@@ -22,6 +21,7 @@ import PersonalInfo from "./assets/Components/Pages/checkout/PersonalInfo";
 import Checkout from "./assets/Components/Pages/checkout/Checkout";
 import Signuplayout from "./assets/Components/Authentication/signuplayout";
 import Signinlayout from "./assets/Components/Authentication/signinlayout";
+import OrderConfirmation from "./assets/Components/Pages/OrderConfirm/OrderConfirmation";
 
 function App() {
   return (
@@ -37,19 +37,19 @@ function App() {
             <Route path="/womens/dresses" element={<WomensDresses />} />
             <Route path="/womens/outerwear" element={<WomensOuterwear />} />
             <Route path="/velocity" element={<Velocity />} />
-            <Route path="/order" element={<Order />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/order-confirm" element={<OrderConfirmation />} />
             <Route path="/cart" element={<Cart />} />
             <Route path="/account" element={<MyAccount />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/jobs" element={<Job />} />
             <Route path="/about" element={<About />} />
             <Route path="/account" element={<MyAccount />} />
-            <Route path="*" element={<NotFound />} />
             <Route path="/products/:productId" element={<Productdetails />} />
             <Route path="/personal-info" element={<PersonalInfo />} />
-            <Route path="/checkout" element={<Checkout />} />
             <Route path="/signin" element={<Signinlayout />} />
             <Route path="/signup" element={<Signuplayout />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </Router>
       </CartProvider>
