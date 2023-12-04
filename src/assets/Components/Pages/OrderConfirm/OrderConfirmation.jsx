b93c01faddca309cadcbb321a9fb3dd1c00aca64
Khizar Sheikh <khizarsheikh.012@gmail.com>
2023-12-04 22:37:26 +0500

    Redesign Order-Conform Pagw

--- src/assets/Components/Pages/OrderConfirm/OrderConfirmation.jsx
+++ src/assets/Components/Pages/OrderConfirm/OrderConfirmation.jsx
@@ -1,18 +1,41 @@
 import { Link } from "react-router-dom";
+import Layout from "../../layout/Layout";
 
 function OrderConfirmation() {
   return (
-    <div>
-      <h1 className="text-5xl font-bold">Your Order Has been Placed </h1>
-      <p className="mt-4 text-2xl">Thank you for shopping with us!</p>
+    <Layout>
+      <div className="bg-gray-100 min-h-screen flex flex-col">
+        <main className="flex-1 container mx-auto py-8">
+          <div className="bg-white p-8 rounded-lg shadow-md">
+            <h1 className="text-5xl font-bold text-center mb-8 text-blue-600">
+              Your Order Has Been Placed!
+            </h1>
+            <p className="text-2xl text-center mb-10">
+              Thank you for shopping with us!
+            </p>
 
-      <div className="">
-        <p>
-          Check Your Order Information
-          <Link to="/order">here</Link>
-        </p>
+            <div className="flex justify-center">
+              <Link
+                to="/order"
+                className="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300"
+              >
+                Check Your Order Information
+              </Link>
+            </div>
+
+            <div className="mt-12 text-center">
+              <p className="text-lg mb-4">Want to continue shopping?</p>
+              <Link
+                to="/"
+                className="inline-block border border-gray-700 text-gray-700 font-semibold py-2 px-4 rounded hover:bg-gray-700 hover:text-white transition duration-300"
+              >
+                Back to Shop
+              </Link>
+            </div>
+          </div>
+        </main>
       </div>
-    </div>
+    </Layout>
   );
 }
 
