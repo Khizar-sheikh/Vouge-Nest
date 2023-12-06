0d29ad0d17f932bd665814485c5c59708b4ac96d
Khizar Sheikh <khizarsheikh.012@gmail.com>
2023-12-06 19:07:32 +0500

    Add skeleton image

--- src/assets/Components/Pages/Components/ProductGrid/ProductGrid.jsx
+++ src/assets/Components/Pages/Components/ProductGrid/ProductGrid.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import getImagePath from "../ProductDetails/Imagepath";
+import { Skeleton } from "antd";
 
 const ProductGrid = ({ products, title }) => {
   const [sortBy, setSortBy] = useState("Featured");
@@ -80,12 +81,15 @@ const ProductGrid = ({ products, title }) => {
           {sortedProducts.map((product, index) => (
             <div key={index} className="self-center justify-self-center">
               <div className=" h-60 w-auto">
-                <img
-                  // src={product1}
-                  src={getImagePath(product.image, product.category)}
-                  alt={product.name}
-                  className="h-full"
-                />
+                {index < 10 ? ( // Show skeleton loader for the first 3 items
+                  <Skeleton.Image style={{ width: "100%", height: "100%" }} />
+                ) : (
+                  <img
+                    src={getImagePath(product.image, product.category)}
+                    alt={product.name}
+                    className="h-full"
+                  />
+                )}
               </div>
               <div className="details">
                 <h3 className="text-sm text-gray-900">{product.name}</h3>
