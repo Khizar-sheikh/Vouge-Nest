0717cd46ef84d76deb4819b207843261b76386e1
Khizar Sheikh <khizarsheikh.012@gmail.com>
2023-12-12 12:10:39 +0500

    Correct Path and NAV_LINK NAME

--- src/assets/Components/navbar/Navbar.jsx
+++ src/assets/Components/navbar/Navbar.jsx
@@ -120,45 +120,47 @@ function Navbar() {
             </div>
             <div>
               <div className="nav-link">
-                <Link to="/order">Order</Link>
+                <Link to="/order">My Orders</Link>
               </div>
             </div>
           </div>
         )}
-        <div className="nav-link-Account">
-          <Link to="/account">
-            {" "}
-            <UserOutlined style={{ fontSize: "30px" }} />{" "}
-          </Link>
-        </div>
-        <div className="">
-          <div className="nav-link cart">
-            {/* Toggle Cart visibility on click */}
-            <span
-              onClick={toggleCart}
-              className="cursor-pointer flex items-center"
-            >
-              <ShoppingCartOutlined
-                style={{
-                  color: "white",
-                  fontSize: "35px",
-                  position: "relative",
-                  zIndex: "10",
-                }}
-              />
-
-              {/* Display number of items in cart */}
-              <span
-                style={{ top: "-10px" }}
-                className="bg-red-500 text-white rounded-full w-6 h-6 flex items-center justify-center  "
-              >
-                {cartItems.length}
-              </span>
-            </span>
+        <div className="flex ">
+          <div className="nav-link-Account px-4">
+            <Link to="/account">
+              {" "}
+              <UserOutlined style={{ fontSize: "30px" }} />{" "}
+            </Link>
           </div>
+          <div className="">
+            <div className="nav-link cart">
+              {/* Toggle Cart visibility on click */}
+              <span
+                onClick={toggleCart}
+                className="cursor-pointer flex items-center"
+              >
+                <ShoppingCartOutlined
+                  style={{
+                    color: "white",
+                    fontSize: "35px",
+                    position: "relative",
+                    zIndex: "10",
+                  }}
+                />
 
-          {/* Render Cart component conditionally based on cart visibility */}
-          {cartVisible && <Cart />}
+                {/* Display number of items in cart */}
+                <span
+                  style={{ top: "-10px" }}
+                  className="bg-red-500 text-white rounded-full w-6 h-6 flex items-center justify-center  "
+                >
+                  {cartItems.length}
+                </span>
+              </span>
+            </div>
+
+            {/* Render Cart component conditionally based on cart visibility */}
+            {cartVisible && <Cart />}
+          </div>
         </div>
       </div>
     </nav>
